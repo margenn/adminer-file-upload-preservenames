5864679dd9f738c3a99536b54035c205dc79ad81
Marcelo Gennari <margenn@gmail.com>
2024-12-06 21:54:12 -0300

    +doc +autoreload +gren

--- README.md
+++ README.md
@@ -1,3 +1,93 @@
-# adminer-file-upload-preservenames
+# Adminer File Upload Gren
 
-Modified version of the plugin. Use this when you need to preserve the original filename.
+Alternative version of the plugin with some customizations
+
+
+## Preserve filenames
+
+The filename is stored with the same name as uploaded by the user
+
+
+## Folder structure mimic database structure
+
+Example:
+
+Database: DBNAME.TABLENAME.FIELDNAME_path
+
+File: `../static/data`/ DBNAME / TABLENAME / FIELDNAME_path / `FILE`
+
+Plugin automatically create subdirs as needed
+
+
+## File's URL in edit interface
+
+So the user can access the file in the registry form.
+
+
+## Block filename by regex
+
+Files that are blocked by default:
+
+- .php or .exe extension
+- starts with dot ` . `
+- files without extension
+
+The regex is parametrized.
+
+
+## Preserve replaced files
+
+If the user upload in the same  DBNAME / TABLENAME / FIELDNAME / FILENAME, the old file is renamed as FILENAME_ymd_His before the new takes it's place.
+
+The user is warned by javascript popup.
+
+
+## Automatically refresh the page if the file was Ajax submitted
+
+When a file is uploaded by ajax in `edit` interface, it's contents only appear in field's link when the page is reloaded, so a small javascript does it.
+
+<br>
+
+# Using:
+
+## index.php
+
+Create an `index.php` with the following content:
+
+```php
+<?php
+function adminer_object() {
+	$pluginsfolder = (basename(__FILE__) == 'index.php') ? '.' : '..';
+	include_once "./plugins/plugin.php"; // required to run any plugin
+	foreach (glob("./plugins/*.php") as $filename) { include_once $filename; } // autoloader
+	$plugins = array(
+		// specify plugins here.
+		new AdminerFileUploadGren()
+	);
+	return new AdminerPlugin($plugins);
+}
+
+include "./your-compiled-or-downloaded-adminer.php";
+```
+
+## Folder structure
+
+A typical deploy looks like this:
+
+```ini
+📂 webserver_root
+├── 📂 adminer
+│   ├── 📄 index.php
+│   ├── 📄 your-compiled-or-downloaded-adminer.php
+│   ├── 📄 adminer.css
+│   ├── 📄 favicon.ico
+│   └── 📂 plugins
+│       ├── 📄 plugin.php
+│       └── 📄 file-upload-gren.php # plugin goes here
+└── 📂 static
+    └── 📂 data # this folder must be writeable by webserver
+        └── 📂 UPLOAD FILE STRUCTURE
+```
+
+
+
--- file-upload-gren.php
+++ file-upload-gren.php
@@ -3,39 +3,41 @@
 /** Edit fields ending with "_path" by <input type="file"> and link to the uploaded files from select
  * @link https://www.adminer.org/plugins/#use
  * @author Jakub Vrana, https://www.vrana.cz/
+ * @author Marcelo Gennari, https://gren.com.br
  * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
- *
- * @author Marcelo Gennari, https://gren.com.br
- * Modifications: Keep the original filename and create sub-folders with the same structure of the database: $uploadPath / db / table / field / $filename
- * @version 1.1.0
+ * @version 2.0.0
 */
 
-class AdminerFileUpload {
+class AdminerFileUploadGren {
 
 	/** @access protected */
-	var $uploadPath, $displayPath, $extensions;
+	var $uploadPath, $displayPath, $extensions, $fileProhibitedRegex, $fieldSufixRegex;
 
 	/**
 	* @param string prefix for uploading data
 	* @param string prefix for displaying data, null stands for $uploadPath
-	* @param string regular expression with allowed file extensions
+	* @param string regular expression with prohibited filename patterns
 	* @param string fields terminated with this regex activates this plugin
 	*/
 
-	function __construct($uploadPath = "../static/data/", $displayPath = null
-			, $extensions = "[a-zA-Z0-9]+", $fieldSufix = '_path') {
-		$this->uploadPath = rtrim($uploadPath, '/');
-		$this->displayPath =  $displayPath ? rtrim($displayPath, '/') : $this->uploadPath;
-		$this->extensions = '~\.' . $extensions . '$~';
-		$this->fieldsufix = '~(.*)' . $fieldSufix . '$~';
+	function __construct(
+			$uploadPath = "../static/data",
+			$displayPath = null,
+			$fileProhibitedRegex = '~\.(exe|php)$|^\.|^[^.]+$~',
+			$fieldSufixRegex = '_path'
+		) {
+			$this->uploadPath = rtrim($uploadPath, '/');
+			$this->displayPath =  $displayPath ? rtrim($displayPath, '/') : $this->uploadPath;
+			$this->fileProhibitedRegex = $fileProhibitedRegex;
+			$this->fieldSufixRegex = '~(.*)' . $fieldSufixRegex . '$~';
 	}
 
 	function editInput($table, $field, $attrs, $value) {
-		if (preg_match($this->fieldsufix, $field["field"])) {
+		if (preg_match($this->fieldSufixRegex, $field["field"])) {
 			$return = '';
 			if (!empty($value)) { // link to the attached file
-				$return .= "<a href='$this->displayPath/$_GET[db]/$table/$field[field]/$value'><div id='attachedfile'>$value</div></a>";
+				$return .= "<a target='_blank' href='$this->displayPath/$_GET[db]/$table/$field[field]/$value'> <div id='attachedfile'>$value</div> </a>";
 			}
 			$return .= "<input type='file'$attrs>";
 			return $return;
@@ -43,14 +45,15 @@ class AdminerFileUpload {
 	}
 
 	function processInput($field, $value, $function = "") {
-		if (preg_match($this->fieldsufix, $field["field"])) {
+		if (preg_match($this->fieldSufixRegex, $field["field"])) {
 			if ($_GET["edit"] != "") {
 				$table = ($_GET["edit"] ? $_GET["edit"] : $_GET["select"]);
 				$filename = $_FILES['fields']['name']["$field[field]"];
 				if ($filename) { // file uploaded?
 					$uploadfullpath = "$this->uploadPath/$_GET[db]/$table/$field[field]";
 					// upload error or file extension not allowed
-					if ($_FILES['fields']['error']["$field[field]"] || !preg_match($this->extensions, $filename)) {
+					if ($_FILES['fields']['error']["$field[field]"] || preg_match($this->fileProhibitedRegex, $filename)) {
+						echo script( "alert('$filename: not uploaded. \\nRule fileProhibitedRegex: $this->fileProhibitedRegex')" );
 						return false;
 					}
 					// create sub-directory if needed
@@ -61,7 +64,8 @@ class AdminerFileUpload {
 					}
 					// if uploaded file already exists, add timestamp on it before move_uploaded_file
 					if (file_exists("$uploadfullpath/$filename")) {
-						if (!rename("$uploadfullpath/$filename", ("$uploadfullpath/$filename" . date("_ymd_His")))) {
+						$filenameRenamedTo = $filename . date("_ymd_His");
+						if ( !rename("$uploadfullpath/$filename", "$uploadfullpath/$filenameRenamedTo") ) {
 							return false;
 						}
 					}
@@ -69,6 +73,10 @@ class AdminerFileUpload {
 					if (!move_uploaded_file($_FILES['fields']['tmp_name']["$field[field]"], "$uploadfullpath/$filename")) {
 						return false;
 					}
+					// file uploaded sucess. check if it was replaced
+					$alertReplace = isset($filenameRenamedTo) ? "alert('$filename was replaced. \\nOld file renamed to $filenameRenamedTo');" : '';
+					// if ajax submitted (still in edit page after submit), reload the page to refresh file contents
+					echo script( "$alertReplace if (new URLSearchParams(window.location.search).has('edit')) { window.location.replace(window.location.href) }" );
 				} else {
 					return false;
 				}
@@ -80,7 +88,7 @@ class AdminerFileUpload {
 	}
 
 	function selectVal($val, &$link, $field, $original) {
-		if ($val != "" && preg_match($this->fieldsufix, $field["field"], $regs)) {
+		if ($val != "" && preg_match($this->fieldSufixRegex, $field["field"], $regs)) {
 			$link = "$this->displayPath/$_GET[db]/$_GET[select]/$field[field]/$val";
 		}
 	}
